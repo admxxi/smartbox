e7b3192e91c6dd2efe797bba2dfbb6bf9c776773
Andre Monteiro <admxxi@gmail.com>
2016-12-21 10:58:06 +0000

    Dealing with the issue #1 [ember-cli] [index] - Foundation.JS has been removed as it is not being used at the moment

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -28,7 +28,5 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-  app.import(app.bowerDirectory + '/foundation-sites/dist/js/foundation.js');
-  app.import(app.bowerDirectory + '/foundation-sites/dist/js/plugins/foundation.equalizer.js');
   return app.toTree();
 };
